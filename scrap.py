392f5d3a33b71f515bbf642d632f085350f8c866
ishant <ishantdahiya@gmail.com>
2020-01-07 22:53:15 +0530

    boongg done, moving to others

--- scrap.py
+++ scrap.py
@@ -2,28 +2,68 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get(
-    'https://www.boongg.com/rent/search/pune/any/any/any?start_date=1578976200000&end_date=1579062600000&timezone=Asia/Calcutta')
-soup = BeautifulSoup(page.content, 'html.parser')
-
-results = soup.find(class_="bike-listing-area")
-
-bike_items = results.find_all(class_="bike-item")
-
-bike_names = [item.find(class_='searchBikeName').get_text()
-              for item in bike_items]
-
-bike_prices = [item.find(class_='actualPrice').get_text()
-               for item in bike_items]
-
-for x in range(0, len(bike_prices)):
-    bike_prices[x] = bike_prices[x].strip('.00')
-    bike_prices[x] = int(bike_prices[x])
-    bike_prices[x] = bike_prices[x]/24
-
-bike_stuff = pd.DataFrame(
-    {'names': bike_names,
-     'prices': bike_prices, }
+boongg_wd = requests.get(
+    'https://www.boongg.com/rent/search/pune/any/any/any?start_date=1578976200000&end_date=1579062600000&timezone=Asia/Calcutta'
 )
-bike_stuff.to_csv('bike.csv')
-# print(bike_stuff)
+boongg_we = requests.get(
+    'https://www.boongg.com/rent/search/pune/any/any/any?start_date=1579321800000&end_date=1579408200000&timezone=Asia/Calcutta'
+)
+
+
+def per_hour(prices):
+    for x in range(0, len(prices)):
+        prices[x] = prices[x].strip('.00')
+        prices[x] = int(prices[x])
+        prices[x] = prices[x]/24
+
+
+def create_df(name, wdPrices, wePrices):
+    df = pd.DataFrame(
+        {
+            'names': name,
+            'WeekDay Prices': wdPrices,
+            'Weekend Prices': wePrices
+        }
+    )
+    return df
+
+
+def names(className, result):
+    bike_names = [item.find(class_=className).get_text()
+                  for item in result]
+    return bike_names
+
+
+def prices(className, result):
+    bike_prices = [item.find(class_=className).get_text()
+                   for item in result]
+    return bike_prices
+
+
+def boongg_scrap(weekday, weekend):
+    wd = BeautifulSoup(weekday.content, 'html.parser')
+    we = BeautifulSoup(weekend.content, 'html.parser')
+
+    results = wd.find(class_="bike-listing-area")
+    results_we = we.find(class_="bike-listing-area")
+
+    bike_items = results.find_all(class_="bike-item")
+    bike_items_we = results_we.find_all(class_="bike-item")
+
+    # print(bike_items_we)
+
+    bike_names = names('searchBikeName', bike_items)
+
+    bike_prices_wd = prices('actualPrice', bike_items)
+    bike_prices_we = prices('actualPrice', bike_items_we)
+
+    per_hour(bike_prices_wd)
+    per_hour(bike_prices_we)
+
+    df = create_df(bike_names, bike_prices_wd, bike_prices_we)
+
+    # print(df)
+    df.to_csv('boongg.csv')
+
+
+boongg_scrap(boongg_wd, boongg_we)
