cba208b5c1525dfb7a6d6daf82654877d0b61d53
ishant <ishantdahiya@gmail.com>
2020-01-07 22:08:23 +0530

    first website done & optimizing and creating functions for all website

--- scrap.py
+++ scrap.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -11,7 +12,18 @@ bike_items = results.find_all(class_="bike-item")
 
 bike_names = [item.find(class_='searchBikeName').get_text()
               for item in bike_items]
-bike_price = [soup.select('.actualPrice span')
-              for item in bike_items]
 
-print(bike_price)
+bike_prices = [item.find(class_='actualPrice').get_text()
+               for item in bike_items]
+
+for x in range(0, len(bike_prices)):
+    bike_prices[x] = bike_prices[x].strip('.00')
+    bike_prices[x] = int(bike_prices[x])
+    bike_prices[x] = bike_prices[x]/24
+
+bike_stuff = pd.DataFrame(
+    {'names': bike_names,
+     'prices': bike_prices, }
+)
+bike_stuff.to_csv('bike.csv')
+# print(bike_stuff)
